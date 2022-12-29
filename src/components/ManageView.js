923201b5235105b14783334abd2bcc6c514ac29f
Nikita <nikitakoulaga@gmail.com>
2022-12-28 19:24:34 -0500

    implemented table view with download link to relative document

--- src/components/ManageView.js
+++ src/components/ManageView.js
@@ -1,8 +1,87 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container, Segment, Form, Grid, Button, Header, Icon } from 'semantic-ui-react';
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import Link from './Link';
+import backendAPI from '../api/backendAPI';
 
 const ManageView = () => {
+    
+    const thstyle = { 
+        borderBottom: '2px solid #ddd',
+        padding: '12px 15px'
+    }
+
+    const tdstyle = {
+        borderBottom: '1.7px solid #ddd',
+        padding: '10px 12px'
+    }
+
+    const tablestyle = {
+        borderCollapse: 'collapse',
+        margin: '25px 0',
+        fontSize: '1em',
+        boxShadow: '0 0 20px rgba(0, 0, 0, 0.15)'
+    }
+
+    const theadstyle = {
+        backgroundColor: '#21ba45',
+        color: '#ffffff',
+        textAlign: 'left'
+    }
+
+    const [requests, setRequests] = useState([]);
+    const url = 'http://localhost:8080/download/';
+
+    const getRequests = async () => {
+        const response = await backendAPI.get('/api/request');
+        setRequests(response.data);
+    }
+
+
+    useEffect(() => {
+        getRequests();
+    }, []);
+
+    const table = () => {
+        if(requests === [] || requests === null || requests.length === 0){
+            return<div>no requests</div>
+        } else {
+            return(
+                <Table style={tablestyle}>
+                    <Thead style={theadstyle}>
+                        <Tr style={{fontSize: '1.3em'}}>
+                            <Th style={thstyle}>Request ID</Th>
+                            <Th style={thstyle}>Name</Th>
+                            <Th style={thstyle}>Email</Th>
+                            <Th style={thstyle}>Employee ID</Th>
+                            <Th style={thstyle}>Department</Th>
+                            <Th style={thstyle}>Employment Status</Th>
+                            <Th style={thstyle}>Document</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {renderedRequests}
+                    </Tbody>
+                </Table >
+            );
+        }
+    }
+
+    const renderedRequests = requests.map(request => {
+        return(
+          <Tr key={request.id} style={{fontSize: '1.2em'}}>
+            <Td style={tdstyle}>{request.id}</Td>
+            <Td style={tdstyle}>{request.name}</Td>
+            <Td style={tdstyle}>{request.email}</Td>
+            <Td style={tdstyle}>{request.employeeId}</Td>
+            <Td style={tdstyle}>{request.department}</Td>
+            <Td style={tdstyle}>{request.employmentStatus}</Td>
+            <Td style={tdstyle}>{<a href={url + `${request.document.id}`}>{request.document.name}</a>}</Td>
+          </Tr>      
+        );
+    });
+
     return(
         <Container style={{marginTop: '1em'}}>
             <Grid divided='vertically' relaxed stackable>
@@ -22,7 +101,7 @@ const ManageView = () => {
                     <Grid.Column>
                         <Segment textAlign='center' style={{border: 'none'}}>
                             <Header as='h2'>
-                                Submit Your Request Below
+                                Manage Requests
                             </Header>
                         </Segment>
                     </Grid.Column>
@@ -31,16 +110,7 @@ const ManageView = () => {
             
 
             <Segment color='red' padded>
-                <Grid divided='vertically' stackable>
-                    <Grid.Row columns={2}>
-                            <Grid.Column>
-                                column
-                            </Grid.Column>
-                            <Grid.Column>
-                                column
-                            </Grid.Column>
-                    </Grid.Row>
-                </Grid>                   
+                {table()}              
             </Segment>
         </Container>
     );
